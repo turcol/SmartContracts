4e289d292d2efd211cf0cae519db88bf57313633
Nayib <turcol@gmail.com>
2023-12-06 13:12:38 -0500

    update tokens name to EXOS

--- contracts/ERC20/WrappedEXOS.sol
+++ contracts/ERC20/WrappedEXOS.sol
@@ -5,5 +5,5 @@ pragma solidity ^0.8.0;
 import "./openzeppelin-contracts/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 contract WrappedNXPI is ERC20 {
-    constructor() public ERC20 ("Wrapped NXPI Token", "wNXPI") {}
+    constructor() public ERC20 ("Wrapped EXOS Token", "wEXOS") {}
 }
